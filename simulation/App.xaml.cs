dd7d2ac62ac3f19767db3cbfe50c93614b029383
IkalEsFe <ifergal1904@g.educaand.es>
2025-02-14 11:40:08 +0000

    aasdfasdf

---
+++ simulation/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace simulation;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ simulation/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace simulation;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
