ba46f676eb27ecbefd392b3b2f9fd3ee12d040ce
IkalEsFe <ifergal1904@g.educaand.es>
2025-02-21 11:42:09 +0000

    asdfasdf

--- web project/main.js
+++ web project/main.js
@@ -1,3 +1,13 @@
+
+var upSlider = document.getElementById("UpChance");
+
 function changeSliderText(text, slider) {
-    document.getElementById(text).textContent = slider.value;
+    var theSlider = document.getElementById(slider);
+    var theText = document.getElementById(text);
+    // theText.textContent = theSlider.attributes.;
 }
+
+console.log(upSlider);
+var sliderUp = changeSliderText("UpText", "UpChance");
+
+upSlider.addEventListener('input', sliderUp);
