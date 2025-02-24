671233a8ab6446bb0f84136c5f381bb31dd97803
IkalEsFe <ifergal1904@g.educaand.es>
2025-02-24 10:12:30 +0000

    qeer

--- web project/main.js
+++ web project/main.js
@@ -1,13 +1,48 @@
+var sliders = document.getElementsByClassName("chance-slider");
+var highestSliderID = 0
+var highestSliderValue = 0
+var totalValue = 0;
 
-var upSlider = document.getElementById("UpChance");
 
 function changeSliderText(text, slider) {
-    var theSlider = document.getElementById(slider);
     var theText = document.getElementById(text);
-    // theText.textContent = theSlider.attributes.;
+    theText.textContent = slider.value;
+    checkSliderValues(slider);
 }
 
-console.log(upSlider);
-var sliderUp = changeSliderText("UpText", "UpChance");
+function checkSliderValues(changingSlider)
+{
+    highestSliderID = 0;
+    highestSliderValue = 0;
+    totalValue = 0;
+    for (let i = 0; i < sliders.length; i++) {
+        const element = sliders[i];
+        if (element.value > highestSliderValue && i != getSliderID(changingSlider.id))
+        {
+            highestSliderID = i;
+            highestSliderValue = element.value;
+        }
+        totalValue += parseInt(element.value);
+    }
+    console.log(highestSliderID);
+    console.log(highestSliderValue);
+    console.log(totalValue);
+    sliders[highestSliderID].value -= (totalValue-100);
 
-upSlider.addEventListener('input', sliderUp);
+}
+
+var getSliderID = function(slider)
+{
+    if (slider == "UpSlider") {
+        return 0;
+    }
+    else if (slider == "DownSlider") {
+        return 1;
+    }
+    else if (slider == "LeftSlider") {
+        return 2;
+    }
+    else if (slider == "RightSlider") {
+        return 3;
+    }
+}
