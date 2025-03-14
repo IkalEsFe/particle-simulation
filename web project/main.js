c191e0db4b3da0e06021b877134d33826cc50e44
IkalEsFe <ifergal1904@g.educaand.es>
2025-03-14 11:39:31 +0000

    lots

--- web project/main.js
+++ web project/main.js
@@ -2,9 +2,11 @@ var sliders = document.getElementsByClassName("chance-slider");
 var highestSliderID = 0
 var highestSliderValue = 0
 var totalValue = 0;
+var changingSliderID = 0;
 
 
 function changeSliderText(text, slider) {
+    console.log(slider);
     var theText = document.getElementById(text);
     theText.textContent = slider.value;
     checkSliderValues(slider);
@@ -12,18 +14,43 @@ function changeSliderText(text, slider) {
 
 function checkSliderValues(changingSlider)
 {
-    highestSliderID = 0;
+    highestSliderID = -1;
     highestSliderValue = 0;
+    changingSliderID = getSliderID(changingSlider.id);
     totalValue = 0;
     for (let i = 0; i < sliders.length; i++) {
-        const element = sliders[i];
-        if (element.value > highestSliderValue && i != getSliderID(changingSlider.id))
-        {
-            highestSliderID = i;
-            highestSliderValue = element.value;
-        }
-        totalValue += parseInt(element.value);
+        totalValue += parseInt(sliders[i].value);
     }
+
+    if(totalValue > 100)
+    {
+        for (let i = 0; i < sliders.length; i++) {
+            var element = sliders[i];
+            if (element.value > highestSliderValue)
+            {
+                if(i != changingSliderID)
+                {
+                    highestSliderID = i;
+                    highestSliderValue = element.value;
+                }
+            }
+        }
+    }
+    else
+    {
+        for (let i = 0; i < sliders.length; i++) {
+            var element = sliders[i];
+            if (element.value < highestSliderValue)
+            {
+                if(i != changingSliderID)
+                {
+                    highestSliderID = i;
+                    highestSliderValue = element.value;
+                }
+            }
+        }
+    }
+
     console.log(highestSliderID);
     console.log(highestSliderValue);
     console.log(totalValue);
@@ -33,16 +60,23 @@ function checkSliderValues(changingSlider)
 
 var getSliderID = function(slider)
 {
-    if (slider == "UpSlider") {
+    console.log(slider);
+    if (slider == "UpChance") {
+        console.log("Return 0");
         return 0;
     }
-    else if (slider == "DownSlider") {
+    else if (slider == "DownChance") {
+        console.log("Return 1");
         return 1;
     }
-    else if (slider == "LeftSlider") {
+    else if (slider == "LeftChance") {
+        console.log("Return 2");
         return 2;
     }
-    else if (slider == "RightSlider") {
+    else if (slider == "RightChance") {
+        console.log("Return 3");
         return 3;
     }
+    console.log("Outside");
+    return 0;
 }
