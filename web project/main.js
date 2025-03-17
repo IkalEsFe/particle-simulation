2b98d8bacc3acf5f86c53b6e17b128f9982b8773
IkalEsFe <ifergal1904@g.educaand.es>
2025-03-17 10:12:02 +0000

    mucho

--- web project/main.js
+++ web project/main.js
@@ -1,21 +1,43 @@
 var sliders = document.getElementsByClassName("chance-slider");
+var sliderTexts = document.getElementsByClassName("result-text");
 var highestSliderID = 0
 var highestSliderValue = 0
+var lowestSliderID = 0
+var lowestSliderValue = 0
 var totalValue = 0;
 var changingSliderID = 0;
+var simCanvas = document.getElementById("simulation-canvas");
+var ctx = simCanvas.getContext("2d");
 
+var columns = 0;
+var rows = 0;
+var height = 0;
+var upChance = 25;
+var downChance = 25;
+var leftChance = 25;
+var rightChance = 25;
+var speed = 0;
 
-function changeSliderText(text, slider) {
+function onSliderChange(slider) {
     console.log(slider);
-    var theText = document.getElementById(text);
-    theText.textContent = slider.value;
     checkSliderValues(slider);
+}
+
+function updateSlidersText()
+{
+    for (let i = 0; i < sliders.length; i++)
+    {
+        var currentText = sliderTexts[i];
+        currentText.textContent = sliders[i].value;
+    }
 }
 
 function checkSliderValues(changingSlider)
 {
     highestSliderID = -1;
     highestSliderValue = 0;
+    lowesSliderID = -1;
+    lowestSliderValue = 100;
     changingSliderID = getSliderID(changingSlider.id);
     totalValue = 0;
     for (let i = 0; i < sliders.length; i++) {
@@ -35,26 +57,28 @@ function checkSliderValues(changingSlider)
                 }
             }
         }
+        sliders[highestSliderID].value -= (totalValue-100);
     }
     else
     {
         for (let i = 0; i < sliders.length; i++) {
             var element = sliders[i];
-            if (element.value < highestSliderValue)
+            if (element.value < lowestSliderValue)
             {
                 if(i != changingSliderID)
                 {
-                    highestSliderID = i;
-                    highestSliderValue = element.value;
+                    lowestSliderID = i;
+                    lowestSliderValue = element.value;
                 }
             }
         }
+        sliders[lowestSliderID].value -= (totalValue-100);
     }
 
     console.log(highestSliderID);
     console.log(highestSliderValue);
     console.log(totalValue);
-    sliders[highestSliderID].value -= (totalValue-100);
+    updateSlidersText();
 
 }
 
@@ -79,4 +103,30 @@ var getSliderID = function(slider)
     }
     console.log("Outside");
     return 0;
+}
+
+function generateSimulation()
+{
+    columns = document.getElementById("columns").value;
+    rows = document.getElementById("rows").value;
+    height = document.getElementById("height").value;
+    upChance = sliders[0].value;
+    downChance = sliders[1].value;
+    leftChance = sliders[2].value;
+    rightChance = sliders[3].value;
+    speed = document.getElementById("speed").value;
+    console.log(columns)
+
+    if (columns == "" || rows == "" || height == "" || speed == "") {
+        alert("Hay valores sin asignar");
+        return;
+    }
+
+    if (height >= rows) {
+        alert("La altura de aparición debe ser menor que la cantidad de filas");
+        return;
+    }
+
+    ctx.createImageData(columns, height);
+
 }
