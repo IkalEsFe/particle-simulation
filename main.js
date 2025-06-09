86fac67aa1dabea4a8e6eaf8e448c73807cb4eda
IkalEsFe <ifergal1904@g.educaand.es>
2025-06-09 11:05:19 +0200

    Added project files

---
+++ main.js
@@ -0,0 +1,323 @@
+var sliders = document.getElementsByClassName("chance-slider");
+var sliderTexts = document.getElementsByClassName("result-text");
+var totalPercentage = document.getElementById("TotalText")
+var simCanvas = document.getElementById("simulation-canvas");
+var simSlider = document.getElementById("SimulationTime");
+var simStarter = document.getElementById("play");
+var importer = document.getElementById("import");
+var generatingText = document.getElementById("generating");
+var ctx = simCanvas.getContext("2d");
+var sizeMultiplier = 10;
+var currentSimLines;
+var playingSim = false
+var simulationFrame = 0
+const simWorker = new Worker("worker.js")
+
+var columns = 0;
+var rows = 0;
+var height = 0;
+var upChance = 25;
+var downChance = 25;
+var leftChance = 25;
+var rightChance = 25;
+var speed = 0;
+
+var file;
+const reader = new FileReader();
+
+var inputs = document.querySelectorAll("input")
+
+inputs.forEach(element => {
+    element.addEventListener("keypress", function (e) {
+    var allowedChars = '0123456789';
+    function contains(stringValue, charValue) {
+        return stringValue.indexOf(charValue) > -1;
+    }
+    var invalidKey = e.key.length === 1 && !contains(allowedChars, e.key)
+            || e.key === '.' && contains(e.target.value, '.');
+    invalidKey && e.preventDefault();});
+});
+
+function onSliderChange(slider) {
+    updateSlidersText()
+}
+
+function onChanceChange(text)
+{
+    if (text.value > 100) text.value = 100
+    if (text.value < 0) text.value = 0
+
+    if (text.id == "UpText")
+    {
+        sliders[0].value = text.value
+    }
+    if (text.id == "DownText")
+    {
+        sliders[1].value = text.value
+    }
+    if (text.id == "LeftText")
+    {
+        sliders[2].value = text.value
+    }
+    if (text.id == "RightText")
+    {
+        sliders[3].value = text.value
+    }
+
+    
+    var total = 0
+    for (let i = 0; i < sliders.length; i++)
+    {
+        total += parseInt(sliders[i].value)
+    }
+    if (total > 100)
+    {
+        totalPercentage.style.color = 'red';
+    }
+    else
+    {
+        totalPercentage.style.color = 'black';
+    }
+    totalPercentage.textContent = total
+}
+
+function updateSlidersText()
+{
+    var total = 0
+    for (let i = 0; i < sliders.length; i++)
+    {
+        var currentText = sliderTexts[i];
+        currentText.value = sliders[i].value;
+        total += parseInt(sliders[i].value)
+    }
+    if (total > 100)
+    {
+        totalPercentage.style.color = 'red';
+    }
+    else
+    {
+        totalPercentage.style.color = 'black';
+    }
+    totalPercentage.textContent = total
+}
+
+var getSliderID = function(slider)
+{
+    if (slider == "UpChance") {
+        return 0;
+    }
+    else if (slider == "DownChance") {
+        return 1;
+    }
+    else if (slider == "LeftChance") {
+        return 2;
+    }
+    else if (slider == "RightChance") {
+        return 3;
+    }
+    return 0;
+}
+
+function setSimulationMoment(slider)
+{
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    var value = 0
+    if (slider == true)
+    {
+        value = parseInt(simSlider.value)
+        simulationFrame = value
+        if (playingSim) togglePlay()
+    }
+    else
+    {
+        value = simulationFrame+1
+        // console.log(value)
+        // console.log(simSlider.max)
+        if (value > parseInt(simSlider.max))
+        {
+            value = parseInt(simSlider.max)
+            simulationFrame = value
+            if (playingSim) togglePlay()
+        }
+    }
+    var moment = parseInt(value)*2
+    var frame = currentSimLines[moment+1]
+    var pixels = frame.split(")")
+    pixels.pop()
+    // console.log(pixels)
+    pixels.forEach(pixel => {
+        var dividedPixel = pixel.split("(")
+        pixel = dividedPixel[1]
+        coords = pixel.split(",")
+        createPixel(coords[0], coords[1])
+    });
+}
+
+function togglePlay()
+{
+    if (playingSim)
+    {
+        simStarter.value = "Play"
+        playingSim = false
+    }
+    else
+    {
+        value = parseInt(simSlider.max)
+        if (simulationFrame >= value)
+        {
+            simulationFrame = 0
+        }
+        // console.log(value)
+        // console.log(simulationFrame)
+        simStarter.value = "Pause"
+        playingSim = true
+        playSimulation()
+    }
+}
+
+const playSimulation = async () => {
+    speed = parseInt(document.getElementById("speed").value);
+    var framesToAdd = 1
+    if (speed > 1000)
+    {
+        framesToAdd = Math.round(speed/1000)
+    }
+    while (playingSim) {
+        setSimulationMoment(false)
+        simSlider.value = simulationFrame
+        simulationFrame += framesToAdd
+        await simulationDelay()
+    }
+}
+
+function loadSimulation()
+{
+    const selectedFile = importer.files[0];
+    if (selectedFile == null)
+    {
+        alert("No hay ningún archivo seleccionado")
+        return
+    }
+    reader.readAsText(selectedFile)
+    reader.addEventListener(
+    "load",
+        () => {
+            // this will then display a text file
+            var lines = reader.result.split("\n")
+            var options = lines[0].split(",")
+            columns = options[0]
+            rows = options[1]
+            var scale = parseInt(document.getElementById("scale").value)
+            console.log(scale)
+            sizeMultiplier = scale
+            simCanvas.width = columns*sizeMultiplier;
+            simCanvas.height = rows*sizeMultiplier;
+            lines.shift()
+            var frame = lines[lines.length-2]
+            var frameNum = parseInt(frame.replace(/^\D+/g, ''));
+            simSlider.max = frameNum
+            currentSimLines = lines
+        },
+        false,
+    );
+    simulationFrame = 0
+    simSlider.disabled = false
+    simStarter.disabled = false
+}
+
+
+
+function createSimulationTextfile()
+{
+    simulationSetup();
+    var values = {
+        "columns": columns,
+        "rows": rows,
+        "particleAmount": particleAmount,
+        "upChance": upChance,
+        "downChance": downChance,
+        "leftChance": leftChance,
+        "rightChance": rightChance,
+        "height": height,
+        "generatingText": generatingText
+    }
+    values = JSON.parse(JSON.stringify(values));
+    simWorker.postMessage(values)
+}
+
+simWorker.onmessage = (e) =>
+{
+    file = e.data
+    downloadSimulation()
+}
+
+function downloadSimulation()
+{
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(file);
+    link.download = "simulation.txt";
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
+
+
+function simulationSetup()
+{
+    columns = parseInt(document.getElementById("columns").value);
+    rows = parseInt(document.getElementById("rows").value);
+    height = parseInt(document.getElementById("height").value);
+    upChance = parseInt(sliders[0].value);
+    downChance = parseInt(sliders[1].value);
+    leftChance = parseInt(sliders[2].value);
+    rightChance = parseInt(sliders[3].value);
+    speed = parseInt(document.getElementById("speed").value);
+    particleAmount = parseInt(document.getElementById("amount").value);
+    if (speed == "") speed = 0
+
+    if (columns == "" || rows == "" || height == "" || amount == "") {
+        alert("Hay valores sin asignar");
+        return;
+    }
+
+    if (height >= rows) {
+        alert("La altura de aparición debe ser menor que la cantidad de filas");
+        return;
+    }
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    downChance = downChance+upChance;
+    leftChance = leftChance+downChance;
+    rightChance = rightChance+leftChance;
+}
+
+
+function createPixelRaw(xPos, yPos, r, g, b, a) {
+    var id = ctx.createImageData(sizeMultiplier, sizeMultiplier);
+    for(var i=0;i<id.data.length/4;i++)
+    {
+        id.data[4*i] = r;
+        id.data[4*i+1] = g;
+        id.data[4*i+2] = b;
+        id.data[4*i+3] = a;
+    }
+    ctx.putImageData(id,xPos*sizeMultiplier,yPos*sizeMultiplier);
+}
+function createPixel(xPos, yPos) {
+    createPixelRaw(xPos, yPos, 255, 0, 0, 255)
+}
+
+function removePixel(xPos, yPos) {
+    createPixelRaw(xPos, yPos, 0, 0, 0, 0)
+}
+
+
+const simulationDelay = async () => {
+    if (speed > 1000)
+    {
+        await delay(1)
+    }
+    else
+    {
+        await delay(1000/speed);
+    }
+};
+const delay = ms => new Promise(res => setTimeout(res, ms));
---
+++ worker.js
@@ -0,0 +1,223 @@
+var file;
+var columns = 0;
+var rows = 0;
+var particleAmount = 0;
+var currentParticlePosX = 0;
+var currentParticlePosY = 0;
+var currentPositionString = 0;
+var occupiedPositionsString = 0;
+var upChance = 25;
+var downChance = 25;
+var leftChance = 25;
+var rightChance = 25;
+var height = 0;
+var freeSpawnPositions = [];
+var occupiedPositions;
+var currentFrame;
+var isParticleCreated = false;
+var generatingText;
+
+onmessage = (e) =>{
+
+    data = e.data;
+    columns = data["columns"]
+    rows = data["rows"]
+    particleAmount = data["particleAmount"]
+    upChance = data["upChance"]
+    downChance = data["downChance"]
+    leftChance = data["leftChance"]
+    rightChance = data["rightChance"]
+    height = data["height"]
+    generatingText = data["generatingText"]
+    isParticleCreated = false
+    freeSpawnPositions = emptyArray()
+    for (let i = 0; i < columns; i++) {
+        freeSpawnPositions.push(i)
+    }
+    occupiedPositions = emptyArray()
+    occupiedPositionsString = ""
+    currentFrame = 0;
+    simulateFile()
+}
+
+const simulateFile = async () => {
+    file = new Blob([columns + "," + rows], { type: "text/plain" })
+    while (particleAmount >= 0)
+    {
+        file = new Blob([file, "\nFrame "+currentFrame])
+        if (!isParticleCreated)
+        {
+            
+            if(particleAmount == 0)
+            {
+                particleAmount--;
+            }
+
+            if(particleAmount > 0)
+            {
+                if (freeSpawnPositions.length == 0)
+                {
+                    height++
+                    if (height >= rows)
+                    {
+                        particleAmount = -1;
+                        return;
+                    }
+                    freeSpawnPositions = emptyArray()
+                    for (let i = 0; i < columns; i++) {
+                        freeSpawnPositions.push(i)
+                    }
+                }
+                particleAmount--;
+                currentParticlePosY = rows-height;
+                currentParticlePosX = freeSpawnPositions[Math.floor(Math.random() * freeSpawnPositions.length)]
+                currentPositionString = `(${currentParticlePosX},${currentParticlePosY})`;
+                isParticleCreated = true;
+            }
+        }
+        else
+        {
+            // if (movementsDone >= maxInstantMovementsFile)
+            // {
+            //     movementsDone = 0;
+            //     await delay(10);
+            // }
+            // movementsDone++;
+            var randomMovement = Math.floor(Math.random()*100)
+            if(randomMovement < upChance)
+                moveParticleUpFile();
+            else if(randomMovement < downChance)
+                moveParticleDownFile();
+            else if(randomMovement < leftChance)
+                moveParticleLeftFile();
+            else if(randomMovement < rightChance)
+                moveParticleRightFile();
+        }
+        file = new Blob([file, "\n"+occupiedPositionsString+currentPositionString], { type: "text/plain"})
+        currentFrame++;
+        generatingText.textContent = "Generando frame: " + currentFrame;
+    }
+    generatingText.text = ""
+    postMessage(file)
+}
+
+
+function moveParticleUpFile()
+{
+    if (!isPositionOccupied(currentParticlePosX, currentParticlePosY-1)) 
+    {
+        if (currentParticlePosY-1 < 0)
+        {
+            currentPositionString = "";
+            isParticleCreated = false;
+        }
+        else
+        {
+            currentParticlePosY--;
+            currentPositionString = `(${currentParticlePosX},${currentParticlePosY})`;
+        }
+    }
+    else
+    {
+        placeParticle()
+    }
+}
+function moveParticleDownFile()
+{
+    if (!isPositionOccupied(currentParticlePosX, currentParticlePosY+1)) 
+    {
+        if (currentParticlePosY+1 >= rows)
+        {
+            currentPositionString = `(${currentParticlePosX},${currentParticlePosY})`;
+            occupiedPositionsString += currentPositionString;
+            occupiedPositions.push([currentParticlePosX, currentParticlePosY])
+            isParticleCreated = false;
+        }
+        else
+        {
+            currentParticlePosY++;
+            currentPositionString = `(${currentParticlePosX},${currentParticlePosY})`;
+        }
+    }
+    else
+    {
+        placeParticle()
+
+    }
+}
+function moveParticleLeftFile()
+{
+    if (!isPositionOccupied(currentParticlePosX-1, currentParticlePosY)) 
+    {
+        if (currentParticlePosX-1 < 0)
+        {
+            currentParticlePosX = columns-1;
+        }
+        else
+        {
+            currentParticlePosX--;
+        }
+        currentPositionString = `(${currentParticlePosX},${currentParticlePosY})`;
+    }
+    else
+    {
+        placeParticle()
+    }
+}
+function moveParticleRightFile()
+{
+    if (!isPositionOccupied(currentParticlePosX+1, currentParticlePosY)) 
+    {
+        if (currentParticlePosX+1 >= columns)
+        {
+            currentParticlePosX = 0;
+        }
+        else
+        {
+            currentParticlePosX++;
+        }
+        currentPositionString = `(${currentParticlePosX},${currentParticlePosY})`;
+    }
+    else
+    {
+        placeParticle()
+    }
+}
+
+function placeParticle()
+{
+    currentPositionString = `(${currentParticlePosX},${currentParticlePosY})`;
+    if (currentParticlePosY == rows-height)
+    {
+        var i = freeSpawnPositions.indexOf(currentParticlePosX)
+        freeSpawnPositions.splice(i, 1)
+    }
+    occupiedPositions.push([currentParticlePosX, currentParticlePosY])
+    occupiedPositionsString += currentPositionString;
+    isParticleCreated = false;
+}
+
+
+function isPositionOccupied(xPos, yPos)
+{
+    for (let i = 0; i < occupiedPositions.length; i++) {
+        if (occupiedPositions[i][0] == xPos && occupiedPositions[i][1] == yPos) {
+            return true
+        }
+    }
+    return false;
+}
+
+const simulationDelay = async () => {
+    if (speed > 1000)
+    {
+        await delay(1)
+    }
+    else
+    {
+        await delay(1000/speed);
+    }
+};
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function emptyArray() { return [] }
